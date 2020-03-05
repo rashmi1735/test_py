02f9ad8263ef0b4f645a0787cc9a53b98b8e4c31
rashmi1735 <42478269+rashmi1735@users.noreply.github.com>
2020-03-05 15:37:09 -0800

    Update tests.py

--- test/tests.py
+++ test/tests.py
@@ -3,9 +3,12 @@ import unittest
 
 class TestSum(unittest.TestCase):
 
-    def test_sum(self):
+    def test_sum1(self):
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
+    def test_sum2(self):
+        self.assertEqual(sum([2, 3, 4]), 9, "Should be 9")
+        
     def test_sum_tuple(self):
         self.assertEqual(sum((1, 2, 2)), 5, "Should be 5")
 
