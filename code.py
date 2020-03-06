9b88345af8bc0daa6582af04a8600681113d6596
rashmi1735 <42478269+rashmi1735@users.noreply.github.com>
2020-03-05 16:22:18 -0800

    Update code.py

--- code.py
+++ code.py
@@ -7,3 +7,9 @@ def sum_only_positive(num1, num2):
         return num1 + num2
     else:
         return None
+    
+def sum_only_negative(num1, num2):
+    if num1 < 0 and num2 < 0:
+        return num1 + num2
+    else:
+        return None
