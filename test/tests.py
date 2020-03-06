5f5b8bbcf9c93151faed7618c0348a5a96662749
rashmi1735 <42478269+rashmi1735@users.noreply.github.com>
2020-03-05 16:01:11 -0800

    Update tests.py

--- test/tests.py
+++ test/tests.py
@@ -1,16 +1,25 @@
 import unittest
-
+from code import sum, sum_only_positive
 
 class TestSum(unittest.TestCase):
 
-    def test_sum1(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+#     def test_sum1(self):
+#         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
-    def test_sum2(self):
-        self.assertEqual(sum([2, 3, 4]), 8, "Should be 9")
+#     def test_sum2(self):
+#         self.assertEqual(sum([2, 3, 4]), 8, "Should be 9")
         
-    def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 2)), 5, "Should be 5")
+#     def test_sum_tuple(self):
+#         self.assertEqual(sum((1, 2, 2)), 5, "Should be 5")
+
+    def test_sum(self):
+        self.assertEqual(sum(5, 5), 10)
+
+    def test_sum_positive_ok(self):
+        self.assertEqual(sum_only_positive(2, 2), 4)
+
+    def test_sum_positive_fail(self):
+        self.assertEqual(sum_only_positive(-1, 2), None)
 
 if __name__ == '__main__':
     unittest.main()
